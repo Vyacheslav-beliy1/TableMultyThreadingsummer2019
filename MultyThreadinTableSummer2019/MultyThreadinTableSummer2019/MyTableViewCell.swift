64ef8831e7ea34427ee0a28dc871708f41fd12e1
Viacheslav Bilyi <>
2019-07-18 19:17:54 +0300

    initial;

---
+++ MultyThreadinTableSummer2019/MultyThreadinTableSummer2019/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  MultyThreadinTableSummer2019
+//
+//  Created by Viacheslav Bilyi on 7/18/19.
+//  Copyright © 2019 Viacheslav Bilyi. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ MultyThreadinTableSummer2019/MultyThreadinTableSummer2019/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MultyThreadinTableSummer2019
+//
+//  Created by Viacheslav Bilyi on 7/18/19.
+//  Copyright © 2019 Viacheslav Bilyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	var images: [String] = ["https://cdn.pixabay.com/photo/2015/05/28/14/53/ux-788002_1280.jpg",
+							"https://cdn.pixabay.com/photo/2015/05/31/15/07/business-792113_1280.jpg",
+							"https://image.shutterstock.com/image-photo/business-cards-mockup-on-color-600w-1034025634.jpg",
+							"https://image.shutterstock.com/z/stock-photo-man-and-woman-working-in-the-office-collaborative-teamwork-374619853.jpg",
+							"https://image.shutterstock.com/z/stock-photo-two-colleagues-interior-designer-discussing-data-and-digital-tablet-and-computer-laptop-with-sample-370233089.jpg",
+							"https://cdn.pixabay.com/photo/2014/05/02/21/49/home-office-336373_1280.jpg"]
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
